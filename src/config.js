533af7aac2f60f09ec8467bd6916efdec8bbef27
Peter Eck <jpeckiii@gmail.com>
2020-05-25 22:23:44 -0500

    Setup Amplify and Cognito

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "sport-subscriber-dev-serverlessdeploymentbucket-13fnbatjmhxen",
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://znxbsyyclk.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_XQuc13TNt",
+        APP_CLIENT_ID: "5nurgptbuuu1ln5ekr4bm8inq9",
+        IDENTITY_POOL_ID: "us-east-1:70371bda-a2b3-4b79-9944-4f2441c25e37"
+    }
+}
