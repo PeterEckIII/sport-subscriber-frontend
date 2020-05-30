565a2357cb4a8444ef179e7b5486b054458b0f8c
Peter Eck <jpeckiii@gmail.com>
2020-05-29 21:51:23 -0500

    Restructure navbar for conditional rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { Auth } from "aws-amplify";
 
 const Nav = styled.nav`
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
   border-bottom: 3px solid #68d391;
   height: 4rem;
+`;
+
+const NavGroupLeft = styled.div`
+    display: flex;
+    align-self: flex-start;
+`;
+
+const NavGroupRight = styled.div`
+    display: flex;
+    align-self: flex-end;
 `;
 
 const StyledLink = styled(Link)`
@@ -27,30 +38,61 @@ const NavChoice = styled.div`
     padding: 5px 10px;
     margin: 5px 5px;
     border-radius: 5px;
+    color: #20BF6B;
+    background-color: white;
 
     &:hover {
         color: white;
-        background-color: #606061;
+        background-color: #20BF6B;
     }
 `;
 
-const Navbar = () => (
-    <div>
-        <Nav>
-            <NavChoice>
-                <StyledLink to="/">Home</StyledLink>
-            </NavChoice>
-            <NavChoice>
-                <StyledLink to="/about">About</StyledLink>
-            </NavChoice>
-            <NavChoice>
-                <StyledLink to="/signup">Sign Up</StyledLink>
-            </NavChoice>
-            <NavChoice>
-                <StyledLink to="/login">Login</StyledLink>
-            </NavChoice>
-        </Nav>
-    </div>
-);
+const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
+    let history = useHistory();
+
+    async function signOut() {
+        try {
+            const signOut = await Auth.signOut();
+            console.log(signOut);
+            setIsAuthenticated(false);
+            history.push('/');
+        } catch (error) {
+            console.log(`Error signing out with ${ error }`)
+        }
+    }
+
+    let userItem;
+    // eslint-disable-next-line no-lone-blocks
+    {
+        isAuthenticated ? userItem = (
+            <NavGroupRight>
+                <NavChoice>
+                    <StyledLink onClick={ signOut } to="/">Logout</StyledLink>
+                </NavChoice>
+            </NavGroupRight>
+        ) : userItem = (
+            <NavGroupRight>
+                <NavChoice>
+                    <StyledLink to="/signup">Sign Up</StyledLink>
+                </NavChoice>
+                <NavChoice>
+                    <StyledLink to="/login">Login</StyledLink>
+                </NavChoice>
+            </NavGroupRight>
+        )
+    }
+    return (
+        <div>
+            <Nav>
+                <NavGroupLeft>
+                    <NavChoice>
+                        <StyledLink to="/">Home</StyledLink>
+                    </NavChoice>
+                </NavGroupLeft>
+                { userItem }
+            </Nav>
+        </div>
+    );
+};
 
 export default Navbar;
