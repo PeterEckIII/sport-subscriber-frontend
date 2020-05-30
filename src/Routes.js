079dbc35aa4960fe5356c10e95c3d54bc2039756
Peter Eck <jpeckiii@gmail.com>
2020-05-29 21:49:43 -0500

    Restructure routes to conditionally render login and logout

--- src/Routes.js
+++ src/Routes.js
@@ -2,17 +2,23 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home';
+import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 
-const Routes = () => (
-    <Switch>
-        <Route exact path="/">
-            <Home />
-        </Route>
-        <Route>
-            <NotFound />
-        </Route>
-    </Switch>
-)
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route exact path="/login">
+                <Login />
+            </Route>
+            <Route>
+                <NotFound />
+            </Route>
+        </Switch>
+    );
+}
 
 export default Routes;
