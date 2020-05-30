6234eddedd45bd2da641ae3eaebe272872788477
Peter Eck <jpeckiii@gmail.com>
2020-05-29 22:28:30 -0500

    Extract components out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Auth } from "aws-amplify";
+
+import NavItem from './NavItem';
+import NavLink from './NavLink';
+import NavGroup from './NavGroup';
 
 const Nav = styled.nav`
   display: flex;
@@ -11,85 +15,44 @@ const Nav = styled.nav`
   height: 4rem;
 `;
 
-const NavGroupLeft = styled.div`
-    display: flex;
-    align-self: flex-start;
-`;
-
-const NavGroupRight = styled.div`
-    display: flex;
-    align-self: flex-end;
-`;
-
-const StyledLink = styled(Link)`
-    text-decoration: none;
-    &:hover {
-        color: white;
-    }
-    &:visited {
-        color: black;
-    }
-    &:active {
-        color: black;
-    }
-`;
-
-const NavChoice = styled.div`
-    padding: 5px 10px;
-    margin: 5px 5px;
-    border-radius: 5px;
-    color: #20BF6B;
-    background-color: white;
-
-    &:hover {
-        color: white;
-        background-color: #20BF6B;
-    }
-`;
-
-const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
+const Navbar = ({ authenticated, setAuthenticated }) => {
     let history = useHistory();
 
-    async function signOut() {
-        try {
-            const signOut = await Auth.signOut();
-            console.log(signOut);
-            setIsAuthenticated(false);
-            history.push('/');
-        } catch (error) {
-            console.log(`Error signing out with ${ error }`)
-        }
+    const handleLogout = () => {
+        setAuthenticated(false);
     }
 
-    let userItem;
+    let authNavItem;
     // eslint-disable-next-line no-lone-blocks
     {
-        isAuthenticated ? userItem = (
-            <NavGroupRight>
-                <NavChoice>
-                    <StyledLink onClick={ signOut } to="/">Logout</StyledLink>
-                </NavChoice>
-            </NavGroupRight>
-        ) : userItem = (
-            <NavGroupRight>
-                <NavChoice>
-                    <StyledLink to="/signup">Sign Up</StyledLink>
-                </NavChoice>
-                <NavChoice>
-                    <StyledLink to="/login">Login</StyledLink>
-                </NavChoice>
-            </NavGroupRight>
+        authenticated ? authNavItem = (
+            <>
+                <NavItem>
+                    <NavLink onClick={ handleLogout } to="/">Logout</NavLink>
+                </NavItem>
+            </>
+        ) : authNavItem = (
+            <>
+                <NavItem>
+                    <NavLink to="/signup">Sign Up</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/login">Login</NavLink>
+                </NavItem>
+            </>
         )
     }
     return (
         <div>
             <Nav>
-                <NavGroupLeft>
-                    <NavChoice>
-                        <StyledLink to="/">Home</StyledLink>
-                    </NavChoice>
-                </NavGroupLeft>
-                { userItem }
+                <NavGroup left>
+                    <NavItem>
+                        <NavLink to="/">Home</NavLink>
+                    </NavItem>
+                </NavGroup>
+                <NavGroup right>
+                    { authNavItem }
+                </NavGroup>
             </Nav>
         </div>
     );
