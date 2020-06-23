e82cf321c1da1abf96237ebf597e0609dd97a953
Peter Eck <jpeckiii@gmail.com>
2020-06-23 17:22:12 -0500

    Modify resource information

--- src/App.js
+++ src/App.js
@@ -17,20 +17,15 @@ Amplify.configure({
     region: config.cognito.REGION,
     userPoolId: config.cognito.USER_POOL_ID,
     identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-  },
-  Storage: {
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
   },
   API: {
     endpoints: [
       {
-        name: 'sport-subscriber-dev',
+        name: "users",
         endpoint: config.apiGateway.URL,
         region: config.apiGateway.REGION
-      },
+      }
     ]
   }
 });
