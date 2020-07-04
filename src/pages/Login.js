e54d8590d2d5ded27c6a92513f6aab1ed6c19de2
Peter Eck <jpeckiii@gmail.com>
2020-07-04 18:21:18 -0500

    Remove form and move to LoginForm

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,11 @@
 import React, { useState } from 'react';
-import styled from 'styled-components'
 import { Auth } from 'aws-amplify';
 import { useHistory } from 'react-router-dom';
-
-import TextField from '../components/TextField';
-import FormButton from '../components/FormButton'
-import ForgotPassword from '../components/ForgotPassword';
-import Loader from '../components/Loader';
 import { useFormFields } from '../libs/hooksLib';
 import { useAppContext } from '../libs/contextLib';
-import { onError } from '../libs/errorLib';
+import { onError } from '../libs/errorLib'
 
-const LoginContainer = styled.div`
-    @media all and (min-width: 480px) {
-        padding: 60px 0;
-    }
-`;
-
-const LoginForm = styled.form`
-    @media all and (min-width: 480px) {
-        margin: 0 auto;
-        max-width: 320px
-    }
-`;
+import LoginForm from '../components/LoginForm';
 
 const Login = () => {
     const history = useHistory();
@@ -54,48 +37,16 @@ const Login = () => {
                 setLoading(false);
             })
     }
-
-    let ui = loading ? (
-        <Loader size={ 20 } margin={ 5 } color={ '#20BF6B' } />
-    ) : (
-            <>
-                <FormButton
-                    loading={ loading }
-                    validate={ validateForm }
-                    onClick={ handleSubmit }
-                >
-                    Login
-                </FormButton>
-            </>
-        )
-
     return (
-        <LoginContainer>
-            <LoginForm>
-                <TextField
-                    htmlFor='email'
-                    labelName="Email"
-                    type="text"
-                    value={ fields.email }
-                    name="email"
-                    placeholder="Santa.Claus@northpole.com"
-                    onChange={ setFields }
-                    autoFocus
-                />
-                <TextField
-                    htmlFor="password"
-                    labelName="Password"
-                    type="password"
-                    value={ fields.password }
-                    name="password"
-                    placeholder="MrsClaus1234"
-                    onChange={ setFields }
-                />
-                { ui }
-                <ForgotPassword />
-            </LoginForm>
-        </LoginContainer>
-    );
+        <LoginForm
+            loading={ loading }
+            validateForm={ validateForm }
+            handleSubmit={ handleSubmit }
+            email={ fields.email }
+            password={ fields.password }
+            setFields={ setFields }
+        />
+    )
 };
 
 export default Login;
