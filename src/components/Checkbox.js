52a963de3fb200a823ee84fb74c09695309d641a
Peter Eck <jpeckiii@gmail.com>
2020-07-05 12:06:27 -0500

    Add and extend Checkbox component

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,15 +2,45 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Input = styled.input`
+    position: relative;
     border: red;
+    padding: 8px 8px;
+    outline: none;
 `;
 
-const Checkbox = ({ checked, onChange }) => (
-    <Input
-        type="checkbox"
-        checked={ checked }
-        onChange={ onChange }
-    />
-)
+const StyledLabel = styled.label`
+    display: block;
+    padding: 7px 0px;
+`;
+
+const StyledField = styled.div`
+    display: flex;
+    margin: 0 auto;
+    padding: 10px 15px;
+`;
+
+const Checkbox = ({
+    htmlFor,
+    labelName,
+    type,
+    checked,
+    onChange,
+    value
+}) => (
+        <StyledField>
+            <div>
+                <Input
+                    type={ type }
+                    value={ value }
+                    checked={ checked }
+                    onChange={ onChange }
+                />
+            </div>
+            &nbsp;
+            <div>
+                <StyledLabel htmlFor={ htmlFor }>{ labelName }</StyledLabel>
+            </div>
+        </StyledField>
+    )
 
 export default Checkbox;
