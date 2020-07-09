9290f079864cbc138b39e754280d057224768f05
Peter Eck <jpeckiii@gmail.com>
2020-07-08 21:42:03 -0500

    Extract logic to SignupForm component

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,42 +1,12 @@
-import React, { useState } from 'react';
-import { useFormFields } from '../libs/hooksLib';
+import React from 'react';
 
 import SignupForm from '../components/Form/Signup/SignupForm';
-import ConfirmationForm from '../components/Form/Signup/ConfirmationForm';
 
 const Signup = () => {
-    const [ newUser, setNewUser ] = useState(null);
-    const [ fields, setFields ] = useFormFields({
-        email: "",
-        password: "",
-        confirmPassword: "",
-        confirmationCode: "",
-    });
-
-    const validateConfirmationForm = () => {
-        return fields.confirmationCode.length > 0;
-    }
 
     return (
         <>
-            {
-                newUser === null ? (
-                    <SignupForm
-                        user={ newUser }
-                        setUser={ setNewUser }
-                        fields={ fields }
-                        setFields={ setFields }
-                        validateConfirmationForm={ validateConfirmationForm }
-                    />
-                ) :
-                    (
-                        <ConfirmationForm
-                            fields={ fields }
-                            setFields={ setFields }
-                            validateConfirmationForm={ validateConfirmationForm }
-                        />
-                    )
-            }
+            <SignupForm />
         </>
     )
 }
