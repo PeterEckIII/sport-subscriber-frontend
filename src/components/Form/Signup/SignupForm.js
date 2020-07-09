cb1306033fb59ad265cc0693bc76ad4c437cb2c5
Peter Eck <jpeckiii@gmail.com>
2020-07-08 21:42:58 -0500

    Move all logic to this component to handle form state

--- src/components/Form/Signup/SignupForm.js
+++ src/components/Form/Signup/SignupForm.js
@@ -1,35 +1,27 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Auth, API } from 'aws-amplify';
-import { useHistory } from 'react-router-dom';
-
-import SubscriptionList from '../Subscription/SubscriptionList';
-import TextField from '../../TextField';
-import FormButton from '../FormButton';
-import Loader from '../../Loader';
-import { useFormFields } from '../../../libs/hooksLib';
-import { useAppContext } from '../../../libs/contextLib';
+import React, { useState, useReducer } from 'react';
+import { Auth } from 'aws-amplify';
+import { useSubscriptionGenerator, useFormFields } from '../../../libs/hooksLib';
 import { onError } from '../../../libs/errorLib';
+import { subscriptionReducer } from '../../../libs/reducerLib';
 
-const Container = styled.div`
-    @media all and (min-width: 480px) {
-        padding: 60px 0;
-    }
-`;
+import ConfirmationForm from './ConfirmationForm';
+import InformationForm from './InformationForm';
 
-const StyledForm = styled.form`
-    @media all and (min-width: 480px) {
-        margin: 0 auto;
-        max-width: 320px
-    }
-`;
-
-const SignupForm = ({ user, setUser, fields, setFields, validateConfirmationForm }) => {
+const SignupForm = () => {
+    const [ user, setUser ] = useState(null);
     const [ loading, setLoading ] = useState(false);
-    const [ checkedItems, setCheckedItems ] = useState({});
-    const [ subscriptions, setSubscriptions ] = useState([]);
-    const history = useHistory();
-    const { setAuthenticated } = useAppContext()
+    const [ subscriptionOptions ] = useSubscriptionGenerator();
+    const [ subscriptions, dispatch ] = useReducer(subscriptionReducer, subscriptionOptions);
+    const [ fields, setFields ] = useFormFields({
+        email: "",
+        password: "",
+        confirmPassword: "",
+        confirmationCode: "",
+    });
+
+    const validateConfirmationForm = () => {
+        return fields.confirmationCode.length > 0;
+    }
 
     const validateForm = () => {
         return (
@@ -62,66 +54,29 @@ const SignupForm = ({ user, setUser, fields, setFields, validateConfirmationForm
             })
     }
 
-    const handleSubscriptionsChange = newSubscription => {
-        setSubscriptions([
-            ...subscriptions,
-            newSubscription
-        ]);
-    }
-
-    let buttonUi = loading ? (
-        <Loader size={ 20 } margin={ 5 } color={ '#20BF6B' } />
-    ) : (
-            <>
-                <FormButton
-                    loading={ loading }
-                    validate={ validateForm }
-                    onClick={ handleSubmit }
-                    disabled={ !validateConfirmationForm }
-                >
-                    Sign Up
-                </FormButton>
-            </>
-        )
-
     return (
-        <Container>
-            <StyledForm onSubmit={ handleSubmit }>
-                <TextField
-                    htmlFor='email'
-                    labelName="Email"
-                    type="text"
-                    value={ fields.email }
-                    name="email"
-                    placeholder="Santa.Claus@northpole.com"
-                    onChange={ setFields }
-                    autoFocus
+        <>
+            { user ? (
+                <ConfirmationForm
+                    fields={ fields }
+                    setFields={ setFields }
+                    loading={ loading }
+                    setLoading={ setLoading }
+                    validateConfirmationForm={ validateConfirmationForm }
                 />
-                <TextField
-                    htmlFor='password'
-                    labelName="Password"
-                    type="password"
-                    value={ fields.password }
-                    name="password"
-                    placeholder="MrsClaus1234"
-                    onChange={ setFields }
-                />
-                <TextField
-                    htmlFor='confirmPassword'
-                    labelName="Confirm Password"
-                    type="password"
-                    value={ fields.confirmPassword }
-                    name="confirmPassword"
-                    placeholder="MrsClaus1234"
-                    onChange={ setFields }
-                />
-                <SubscriptionList
-                    subscriptions={ subscriptions }
-                    handleSubscriptionsChange={ handleSubscriptionsChange }
-                />
-            </StyledForm>
-            { buttonUi }
-        </Container>
+            ) : (
+                    <InformationForm
+                        fields={ fields }
+                        setFields={ setFields }
+                        loading={ loading }
+                        handleSubmit={ handleSubmit }
+                        validateForm={ validateForm }
+                        validateConfirmationForm={ validateConfirmationForm }
+                        subscriptions={ subscriptions }
+                        dispatch={ dispatch }
+                    />
+                ) }
+        </>
     )
 }
 
