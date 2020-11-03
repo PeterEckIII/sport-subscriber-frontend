804f9b940e0ffd563e0e42d622753deb2588a0fc
Peter Eck <jpeckiii@gmail.com>
2020-11-03 12:35:15 -0600

    Create user context

--- src/libs/contextLib.js
+++ src/libs/contextLib.js
@@ -1,9 +1,16 @@
 import { useContext, createContext } from 'react';
 
+// App Context
 export const AppContext = createContext(null);
 
 export const useAppContext = () => useContext(AppContext);
 
+// Subscription Context
 export const SubscriptionContext = createContext([]);
 
 export const useSubscriptionContext = () => useContext(SubscriptionContext);
+
+// User Context
+export const UserContext = createContext(null);
+
+export const useUserContext = () => useContext(UserContext)
