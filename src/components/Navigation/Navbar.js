3e49411dfa3b9a422a9d3ac9fdbfd8ccb4393853
Peter Eck <jpeckiii@gmail.com>
2021-01-15 16:12:57 -0600

    Replace AWS signout with useAuth hook

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,87 +1,79 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { Auth } from 'aws-amplify';
 import { useHistory } from 'react-router-dom';
 
 import { UserContext } from '../../libs/contextLib';
+import { useAuth } from '../../libs/authLib';
 import NavItem from './NavItem';
 import NavLink from './NavLink';
 import NavGroup from './NavGroup';
 
 const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-bottom: 3px solid #68d391;
-  height: 4rem;
-  cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: space-between;
+border-bottom: 3px solid #68d391;
+height: 4rem;
+cursor: pointer;
 `;
 
 const LogoutButton = styled.button`
-    font-size: 100%;
-    font-family: inherit;
-    border: 0;
-    padding: 0;
-    background-color: transparent;
-    cursor: pointer;
+font-size: 100%;
+font-family: inherit;
+border: 0;
+padding: 0;
+background-color: transparent;
+cursor: pointer;
 `;
 
 const Navbar = ({ authenticated, setAuthenticated }) => {
-    const history = useHistory();
-    const [user] = useContext(UserContext);
+  const history = useHistory();
+  const [user] = useContext(UserContext);
+  const { signout } = useAuth();
 
-    const handleLogout = () => {
-        Auth
-            .signOut()
-            .then(res => {
-                console.log(`Signed out user ${ JSON.stringify(res) }`);
-            })
-            .catch(e => {
-                console.log(`Error signing out: ${ e }`)
-            })
-            .finally(_ => {
-                history.push('/')
-                setAuthenticated(false);
-            })
-    }
+  const handleLogout = async () => {
+    await signout();
+    history.push('/')
+    setAuthenticated(false);
+  }
 
-    let authNavItem;
-    // eslint-disable-next-line no-lone-blocks
-    {
-        authenticated ? authNavItem = (
-            <>
-                <NavItem>
-                    <LogoutButton onClick={ handleLogout }>Logout</LogoutButton>
-                </NavItem>
-                <NavItem>
-                    <NavLink to={`/profile/${user}`}>Profile</NavLink>
-                </NavItem>
-            </>
-        ) : authNavItem = (
-            <>
-                <NavItem>
-                    <NavLink to="/signup">Sign Up</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink to="/login">Login</NavLink>
-                </NavItem>
-            </>
-        )
-    }
-    return (
-        <div>
-            <Nav>
-                <NavGroup left>
-                    <NavItem>
-                        <NavLink to="/">Home</NavLink>
-                    </NavItem>
-                </NavGroup>
-                <NavGroup right>
-                    { authNavItem }
-                </NavGroup>
-            </Nav>
-        </div>
-    );
+  let authNavItem;
+  // eslint-disable-next-line no-lone-blocks
+  {
+    authenticated ? authNavItem = (
+      <>
+        <NavItem>
+          <LogoutButton onClick={ handleLogout }>Logout</LogoutButton>
+        </NavItem>
+        <NavItem>
+          <NavLink to={`/profile/${user}`}>Profile</NavLink>
+        </NavItem>
+      </>
+    ) : authNavItem = (
+      <>
+        <NavItem>
+          <NavLink to="/signup">Sign Up</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/login">Login</NavLink>
+        </NavItem>
+      </>
+    )
+  }
+  return (
+    <div>
+      <Nav>
+        <NavGroup left>
+          <NavItem>
+            <NavLink to="/">Home</NavLink>
+          </NavItem>
+        </NavGroup>
+        <NavGroup right>
+          { authNavItem }
+        </NavGroup>
+      </Nav>
+    </div>
+  );
 };
 
 export default Navbar;
